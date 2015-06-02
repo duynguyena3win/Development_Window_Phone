9e8648485ba9181d978aefccd844344fa3d4fc4e
duynguyen.it.khtn <duynguyen.it.khtn@gmail.com>
2015-06-02 20:44:58 +0700

    + demo project for Protect Phone app, Window phoneApplicationInitialized

---
+++ ProtectApps/GaurdApp.Core/LockScreenHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework.Media;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace GaurdApp.Core
+{
+    public class LockScreenHelper
+    {
+        public LockScreenHelper()
+        {
+
+        }
+
+        public static async Task HelloWP()
+        {
+            Debug.WriteLine("Work at: " + DateTime.Now.ToLongTimeString() );
+        }
+    }
+}
---
+++ ProtectApps/GuardApp.Scheduled/ScheduledAgent.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using Microsoft.Phone.Scheduler;
+using System;
+using GaurdApp.Core;
+
+namespace GuardApp.Scheduled
+{
+    public class ScheduledAgent : ScheduledTaskAgent
+    {
+        /// <remarks>
+        /// ScheduledAgent constructor, initializes the UnhandledException handler
+        /// </remarks>
+        static ScheduledAgent()
+        {
+            // Subscribe to the managed exception handler
+            Deployment.Current.Dispatcher.BeginInvoke(delegate
+            {
+                Application.Current.UnhandledException += UnhandledException;
+            });
+        }
+
+        /// Code to execute on Unhandled Exceptions
+        private static void UnhandledException(object sender, ApplicationUnhandledExceptionEventArgs e)
+        {
+            if (Debugger.IsAttached)
+            {
+                // An unhandled exception has occurred; break into the debugger
+                Debugger.Break();
+            }
+        }
+
+        /// <summary>
+        /// Agent that runs a scheduled task
+        /// </summary>
+        /// <param name="task">
+        /// The invoked task
+        /// </param>
+        /// <remarks>
+        /// This method is called when a periodic or resource intensive task is invoked
+        /// </remarks>
+        protected async override void OnInvoke(ScheduledTask task)
+        {
+            //TODO: Add code to perform your task in background
+            await LockScreenHelper.HelloWP();
+
+            ScheduledActionService.LaunchForTest(task.Name, TimeSpan.FromSeconds(30));
+            NotifyComplete();
+        }
+
+        protected async override void OnCancel()
+        {
+            Debug.WriteLine("Stop Background !");
+        }
+    }
+}
